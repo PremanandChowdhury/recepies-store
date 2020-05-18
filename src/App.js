f4d5f5fbd262420cf5eaf80c9e4fd5a4462975a5
PremanandChowdhuy <freudigprem@gmail.com>
2020-05-18 23:48:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import logo from "./logo.svg";
+import "./css/App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;

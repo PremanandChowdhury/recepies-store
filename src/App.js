6d01f43176988e8eeb73b6eb43065d186da0bd98
PremanandChowdhuy <freudigprem@gmail.com>
2020-05-19 02:04:05 +0530

    navbar component  created

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import Home from "./pages/Home";
 import Recepies from "./pages/Recepies";
 import Recepie from "./pages/Recepie";
 import Oops from "./pages/Oops";
-import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
       <main>
         {/* Navbar */}
+        <Navbar />
 
         <Switch>
           <Route path="/" exact component={Home} />

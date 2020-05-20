2c9274c66cdbe61fa5ad5673d9a45d2bf15b46f3
PremanandChowdhuy <freudigprem@gmail.com>
2020-05-21 00:33:47 +0530

    search query functionality

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -6,12 +6,18 @@ import { recipeData } from "../data/tempList";
 export default class Recipes extends Component {
   constructor(props) {
     super(props);
+    this.getRecipes = this.getRecipes.bind(this);
   }
 
+  // STATE
   state = {
     recipes: recipeData,
     search: "",
     url: `https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`,
+
+    base_url: `https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`,
+    query: "&q=",
+    error: "",
   };
 
   // getRecipes starts here
@@ -53,6 +59,15 @@ export default class Recipes extends Component {
   // ON SUBMIT
   handleSubmit = (e) => {
     e.preventDefault();
+    // AJAX Call for the searched items
+    const { base_url, query, search } = this.state;
+    this.setState(
+      {
+        url: `${base_url}${query}${search}`,
+        search: "",
+      },
+      () => this.getRecipes()
+    );
   };
 
   render() {
@@ -63,7 +78,21 @@ export default class Recipes extends Component {
           handleChange={this.handleChange}
           handleSubmit={this.handleSubmit}
         />
-        <RecipeList recipes={this.state.recipes} />
+        {/* Conditional rendering */}
+
+        {this.state.error ? (
+          <section>
+            <div className="row">
+              <div className="col">
+                <h2 className="text-orange text-center text-uppercase mt-5">
+                  {this.state.error}
+                </h2>
+              </div>
+            </div>
+          </section>
+        ) : (
+          <RecipeList recipes={this.state.recipes} />
+        )}
       </>
     );
   }

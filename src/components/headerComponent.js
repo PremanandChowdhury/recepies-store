322237978d13f87eeb5a5d8a90fdf57670f169dd
PremanandChowdhury <freudigprem@gmail.com>
2021-08-27 12:57:37 +0530

    style: Responsive styling added

--- src/components/headerComponent.js
+++ src/components/headerComponent.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { device } from './deviceSizes'
 
 export const Header = styled.div`
   color: white;
@@ -6,7 +7,7 @@ export const Header = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 20px;
+  padding: 12px;
   font-size: 25px;
   font-weight: bold;
   box-shadow: 0 3px 6px 0 #555;
@@ -15,11 +16,26 @@ export const Header = styled.div`
 export const LogoComponent = styled.div`
   display: flex;
   align-items: center;
+
+  @media ${device.mobileS} {
+    font-size: 15px;
+    margin: 0;
+  }
+  @media ${device.mobileL} {
+    font-size: 18px;
+    margin: 0;
+  }
 `
 export const AppIcon = styled.img`
   width: 36px;
   height: 36px;
   margin: 15px;
+
+  @media ${device.mobileS} {
+    width: 30px;
+    height: 30px;
+    margin: 8px;
+  }
 `
 export const SearchComponent = styled.div`
   display: flex;
@@ -27,10 +43,20 @@ export const SearchComponent = styled.div`
   padding: 10px;
   border-radius: 6px;
   width: 50%;
+
+  @media ${device.mobileS} {
+    padding: 8px;
+    width: 60%;
+  }
 `
 export const SearchIcon = styled.img`
   width: 32px;
   height: 32px;
+
+  @media ${device.mobileS} {
+    width: 25px;
+    height: 25px;
+  }
 `
 
 export const SearchInput = styled.input`
@@ -40,4 +66,13 @@ export const SearchInput = styled.input`
   font-size: 20px;
   font-weight: bold;
   width: 100%;
+
+  @media ${device.mobileS} {
+    margin-left: 10px;
+    font-size: 14px;
+  }
+  @media ${device.mobileL} {
+    margin-left: 10px;
+    font-size: 14px;
+  }
 `
--- src/components/recipeComponent.js
+++ src/components/recipeComponent.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const RecipeListContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
-  padding: 30px;
+  padding: 15px;
   gap: 20px;
   justify-content: space-evenly;
 `
@@ -11,14 +11,16 @@ export const RecipeListContainer = styled.div`
 export const RecipeContainer = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 30px;
+  padding: 15px;
   width: 250px;
   box-shadow: 0 3px 10px 0 #aaa;
-  border-radius: 6px;
+  border-radius: 4px;
 `
 
 export const ItemImage = styled.img`
   height: 200px;
+  object-fit: cover;
+  border-radius: 4px;
 `
 
 export const RecipeName = styled.span`
@@ -30,8 +32,8 @@ export const RecipeName = styled.span`
 export const IngredientsLabel = styled.span`
   font-size: 18px;
   font-weight: bold;
-  border: solid 1px green;
-  color: green;
+  border: solid 1px #006800;
+  color: #006800;
   margin-bottom: 12px;
   padding: 10px 15px;
   background-color: white;
@@ -41,6 +43,6 @@ export const IngredientsLabel = styled.span`
 `
 
 export const SeeMoreRecipeLabel = styled(IngredientsLabel)`
-  color: #eb3300;
-  border: solid 1px #eb3300;
+  color: #aa2500;
+  border: solid 1px #aa2500;
 `
